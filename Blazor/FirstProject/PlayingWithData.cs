521ac5e9be2df1243aecbb9f761530fcf24447ab
DavidBasarab <dbasarab@haivision.com>
2025-02-19 16:10:10 -0500

    playing with how stuff works on the counter

---
+++ Blazor/FirstProject/PlayingWithData.cs
@@ -0,0 +1,12 @@
+using FatCat.Toolkit;
+
+namespace FirstProject;
+
+public class PlayingWithData : EqualObject
+{
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
+
+    public int SomeNumber { get; set; }
+}
